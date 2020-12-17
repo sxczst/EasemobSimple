25b01f6290249aab7f6628958efed063e50bd5c9
sxczst <sxczst@aliyun.com>
2020-12-17 20:51:52 +0800

    glance： 添加依赖一个简单方便的Android数据库调试库。 multiDex： 项目中方法总数超过了单个DEX文件中可引用的方法总数限制，即25536。 解决方法：配置为生成多个DEX。

--- app/src/main/java/org/sxczst/im/IMApplication.kt
+++ app/src/main/java/org/sxczst/im/IMApplication.kt
@@ -1,6 +1,8 @@
 package org.sxczst.im
 
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
 import com.hyphenate.chat.EMOptions
 import com.hyphenate.easeui.EaseUI
 import org.sxczst.im.model.Model
@@ -18,5 +20,11 @@ class IMApplication : Application() {
         options.isAutoAcceptGroupInvitation = false
         EaseUI.getInstance().init(this, options)
         Model.instance.initialize(this)
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        // 配置为生成多个DEX。
+        MultiDex.install(this)
     }
 }
